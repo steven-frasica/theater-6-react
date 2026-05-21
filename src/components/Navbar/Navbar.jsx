e3410355a31c1acfab76f4bf7402d1a2a9f27c5e
steven-frasica <steven.frasica@gmail.com>
2026-05-21 13:29:26 -0400

    work on Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="hero">
+      <h1>Theater 6</h1>
+      <h2>Search for your next viewing experience</h2>
+      <div className="hero__controls">
+        <div className="search-bar">
+          <input id="search-input" type="text" placeholder="Type to Search"/>
+          <button className="search-bar__clear">&times;</button>
+        </div>
+        <select name="sort" id="sort-select" className="sort-select">
+          <option value="" disabled selected>Sort</option>
+          <option value="NEW_TO_OLD">Newest First</option>
+          <option value="OLD_TO_NEW">Oldest First</option>
+          <option value="RATING_HIGH_TO_LOW">Rating, High to Low</option>
+          <option value="RATING_LOW_TO_HIGH">Rating, Low to High</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
